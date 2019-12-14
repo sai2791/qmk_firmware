1d4d9a18fbe193bbac92aa656968e6f555ad74f5
Steve Inglis <sai2791@googlemail.com>
2019-12-14 11:29:41 +0000

    changes to unigo66 keyboard layout to make it more like the ergodox, I cant get used to the enter key being on the side again

--- keyboards/sirius/unigo66/keymaps/sai2791/keymap.c
+++ keyboards/sirius/unigo66/keymaps/sai2791/keymap.c
@@ -4,30 +4,30 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] PROGMEM = {
 /* 
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |  -   |           |  =   |   6  |   7  |   8  |   9  |   0  |  Bsp   |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  -   |           |  =   |   6  |   7  |   8  |   9  |   0  |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  | PgUp |           |  [   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |        |   Q  |   W  |   E  |   R  |   T  | PgUp |           |  [   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | Caps   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  | Enter  |
+ * |        |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |        |
  * |--------+------+------+------+------+------| PgDn |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  Up  |M2/Shift|
  * `--------+------+------+---------------------------'           `---------------------------+------+------+--------'
  *   | Ctrl |  Gui |  Alt |                                                                   | Left |  Down| Right |
  *   `--------------------'                                                                   `--------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      |  Ins |       | Home |        |  
- *                                        | Space|------|       |------|M1/Space| 
- *                                        |      |  Del |       | End  |        | 
+ *                                        |      |      |       |      |        |  
+ *                                        | Space| Bsp  |       | Tab  |M1/Enter| 
+ *                                        |      |      |       |      |        | 
  *                                        `-------------'       `---------------'
  */
     [0] = LAYOUT(
-  KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,      KC_EQL,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_BSPC,
-  KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_PGUP,      KC_LBRC, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
-  KC_CAPS, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENT, 
+  KC_ESC,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_MINS,      KC_EQL,  KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_NO,
+  KC_NO,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_PGUP,      KC_LBRC, KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
+  KC_NO  , KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                          KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_NO, 
   KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_PGDN,      KC_RBRC, KC_N,   KC_M,   KC_COMM,KC_DOT, KC_UP,   LM(2,MOD_RSFT),
   KC_LCTL, KC_LGUI,KC_LALT,                                                                       KC_LEFT,KC_DOWN, KC_RGHT,
-                                           KC_SPC, KC_INS,       KC_HOME, LT(1,KC_SPC),
-                                           KC_NO , KC_DEL,       KC_END,  KC_NO
+                                           KC_SPC, KC_BSPC,      KC_TAB, LT(1,KC_ENT),
+                                           KC_NO , KC_BSPC,      KC_TAB,  KC_NO
     ),
 /* 
  *
